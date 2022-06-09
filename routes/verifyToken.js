3c6b8d802e6701abcd23798548ae17e8a3dbec27
kuzu2000 <swan.t.zaw@gmail.com>
2022-06-09 23:11:38 +0630

    token

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,27 +1,32 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers.token;
-  if (authHeader) {
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-      if (err) res.status(403).json("Token is not valid!");
-      req.user = user;
-      next();
-    });
+  const authorization = req.headers.token;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || 'secret',
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
   } else {
-    return res.status(401).json("You are not authenticated!");
+    res.status(401).send({ message: 'No Token' });
   }
-  return
 };
 
 const verifyTokenAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
-      
     } else {
-      res.status(403).json("You are not alowed to do that!");
+      res.status(403).json('You are not alowed to do that!');
     }
   });
 };
@@ -31,7 +36,7 @@ const verifyTokenAndAdmin = (req, res, next) => {
     if (req.user.isAdmin) {
       next();
     } else {
-      res.status(403).json("You are not alowed to do that!");
+      res.status(403).json('You are not alowed to do that!');
     }
   });
 };
